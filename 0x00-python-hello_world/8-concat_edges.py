98198b856535350876f96fed9ac6b064ec6d7db7
HazelDaniel <hazeldaniel856@gmail.com>
2023-06-05 18:42:09 +0100

    correction to the task 8

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-first_word = str[39:(39 + len("object-oriented programming "))]
-middle_word = str[-22:-17]
-last_word = str[0:6]
-print(f"{first_word}{middle_word}{last_word}")
+str = str[39:(39 + 28)] + str[-22:-17] + str[0:6]
+print(f"{str}")
