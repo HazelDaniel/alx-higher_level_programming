dc0765fe6b7e8be2701c8f39928a3ba390de517b
HazelDaniel <hazeldaniel856@gmail.com>
2023-06-06 20:37:58 +0100

    correction to the task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
+string = "{}"
+
 
 def uppercase(str):
     for i in range(0, len(str)):
-        if (ord(str[i]) >= 97 and ord(str[i]) <= 122):
-            print("{}".format(chr(ord(str[i]) - 32)), end="")
+        if (i == len(str) - 1):
+            global string
+            string = "{}\n"
         else:
-            print("{}".format(str[i]), end="")
-    print("")
+            string = "{}"
+        if (ord(str[i]) >= 97 and ord(str[i]) <= 122):
+            print(string.format(chr(ord(str[i]) - 32)), end="")
+        else:
+            print(string.format(str[i]), end="")
