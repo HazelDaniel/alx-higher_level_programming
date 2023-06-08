465449502dd8eefe37e15233eb4abadb2f10a93e
HazelDaniel <hazeldaniel856@gmail.com>
2023-06-08 20:47:43 +0100

    submission for the task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import sys
+
+pl = ""
+count = 0
+header_end = "."
+arg_len = len(sys.argv)
+if __name__ == "__main__":
+    count = arg_len - 1
+    if count > 1:
+        pl = "s"
+        header_end = ":"
+    elif count == 1:
+        header_end = ":"
+    print("{} argument".format(count) + pl + header_end)
+    for i in range(count):
+        if i == count:
+            print("{}: {}".format(i + 1, sys.argv[i + 1]), end="")
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+    pass
