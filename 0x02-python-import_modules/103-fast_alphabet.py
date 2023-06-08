079d0f5767eb0e4795b67cc1090985e33afb595e
HazelDaniel <hazeldaniel856@gmail.com>
2023-06-08 22:46:57 +0100

    correction to the task 103

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-def print_alphabet(current):
-    print(chr(current), end='')
-    current + 1 < ord('Z') and print_alphabet(current + 1)
-
-
-print_alphabet(ord('A'))
+import string
+print(string.ascii_uppercase)
