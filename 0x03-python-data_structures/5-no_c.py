b45e4d2ff63d362ee23ec244ad496e65355a8b7e
HazelDaniel <hazeldaniel856@gmail.com>
2023-06-09 09:55:19 +0100

    submission for tasks 5 and 6

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
 
+
+def no_c(my_string):
+    ret = ""
+    for i in range(len(my_string)):
+        if my_string[i] == 'C' or my_string[i] == 'c':
+            continue
+        else:
+            ret += my_string[i]
+    return ret
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
 
+
+def print_matrix_integer(matrix=[[]]):
+    if (not matrix) or matrix is None:
+        return None
+    tmp_print = [row[i] for row in matrix for i in range(len(row))]
+    for i in range(len(tmp_print)):
+        arr_len = len(matrix[0])
+        if (i + 1) % (arr_len) == 0:
+            print("{}".format(tmp_print[i]), end="\n")
+        else:
+            print("{}".format(tmp_print[i]), end=" ")
