167a7b6d9a38914d342f309c5ccfd4670b0b869c
HazelDaniel <hazeldaniel856@gmail.com>
2023-06-14 18:49:41 +0100

    submission for the task 100

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,12 @@
+#!/usr/python3
+
+def weight_average(my_list=[]):
+    sum = 0
+    sum_div = 0
+    for (x, y) in my_list:
+        print((sum_div, y))
+        sum += (x * y)
+        sum_div += y
+    if sum_div == 0:
+        sum_div = 1
+    return (sum / sum_div)
