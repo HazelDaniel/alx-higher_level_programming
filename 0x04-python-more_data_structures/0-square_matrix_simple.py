0455cc89fa8204ef6550516ed4b7f7a0282dae6b
HazelDaniel <hazeldaniel856@gmail.com>
2023-06-14 18:30:40 +0100

    correction to the task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,15 @@
 #!/usr/python3
+# def square_matrix_simple(matrix=[]):
+#     return [[x ** 2 for x in row] for row in matrix]
+
 def square_matrix_simple(matrix=[]):
-    return [[x ** 2 for x in row] for row in matrix]
+    new_matrix = []
+    for row in matrix:
+        row_arr = list()
+        for col in row:
+            row_arr.append(col ** 2)
+        if row_arr:
+            new_matrix.append(row_arr)
+    if not matrix:
+        return None
+    return new_matrix
