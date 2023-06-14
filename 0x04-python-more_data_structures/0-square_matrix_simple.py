d76d9610f89d601469a612cc87e7ff6da7518f5d
HazelDaniel <hazeldaniel856@gmail.com>
2023-06-14 18:36:43 +0100

    re-submission for the task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,18 @@
 #!/usr/python3
+# def square_matrix_simple(matrix=[]):
+#     new_matrix = []
+#     for row in matrix:
+#         row_arr = list(map(lambda x: x ** 2, row))
+#         new_matrix.append(row_arr)
+#     if not matrix:
+#         return None
+#     return new_matrix
+
+
 def square_matrix_simple(matrix=[]):
+    if len(matrix) == 0 or matrix is None:
+        return (None)
     new_matrix = []
-    for row in matrix:
-        row_arr = list(map(lambda x: x ** 2, row))
-        new_matrix.append(row_arr)
-    if not matrix:
-        return None
-    return new_matrix
+    for each in matrix:
+        new_matrix.append([x ** 2 for x in each])
+    return (new_matrix)
