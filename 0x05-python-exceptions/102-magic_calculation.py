c7f004dd6c6de665c5a4bf888c847af4e1f535b2
HazelDaniel <hazeldaniel856@gmail.com>
2023-06-26 11:33:28 +0100

    submission for the task 102

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,1 +1,17 @@
 #!/bin/python3
+
+def magic_calculation(a, b):
+    result = 0
+
+    for i in range(1, 3):
+        try:
+            if (i > a):
+                raise Exception("too far")
+            else:
+                result += (result ** a ** b) / i
+        except Exception:
+            result = a + b
+            break
+        finally:
+            pass
+    return result
