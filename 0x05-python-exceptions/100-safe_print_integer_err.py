9ed31d3aedd1553a5cfdcfebbd1d48e3bae9fc0d
HazelDaniel <hazeldaniel856@gmail.com>
2023-06-26 10:14:57 +0100

    submission/re-submission for tasks 101 and 100

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -8,7 +8,7 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
     except (TypeError, ValueError) as e:
-        print("Exception: ", e, file=sys.stderr)
+        print("Exception:", e, file=sys.stderr)
         ret = False
     else:
         ret = True
--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,1 +1,13 @@
 #!/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    ret = None
+    try:
+        ret = fct(*args)
+    except Exception as e:
+        print("Exception:", e, file=sys.stderr)
+        ret = None
+    else:
+        return ret
