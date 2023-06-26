c21a568b70027c8893a3f608a3e7b537b9acd01e
HazelDaniel <hazeldaniel856@gmail.com>
2023-06-26 13:37:45 +0100

    re-submission for task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,15 @@
-#!/bin/python3
+#!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    y = 0
-
-    for i in range(0, x):
-        try:
+    if my_list is None or x < 0:
+        return (0)
+    i = 0
+    try:
+        while i < x:
             print("{}".format(my_list[i]), end="")
-        except IndexError:
-            break
-        else:
-            y += 1
+            i += 1
+    except IndexError:
+        print("")
+        return (i)
     print("")
-    return y
+    return (i)
