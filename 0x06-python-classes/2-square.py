691857c8c6b916efb0d842e3e8228d45e02bd9df
HazelDaniel <hazeldaniel856@gmail.com>
2023-06-27 17:08:04 +0100

    submission for the task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+""" A module that defines a square class and initializes its fields """
+
+
+class Square:
+    """ a class that defines a square """
+    def __init__(self, size=0):
+        if (type(size) != int):
+            raise TypeError("size must be an integer")
+        if (size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = size
