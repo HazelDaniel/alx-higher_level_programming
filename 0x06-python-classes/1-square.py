cbf79b4651f8522c98968d5f3acfd17f47ca9a9e
HazelDaniel <hazeldaniel856@gmail.com>
2023-06-27 17:01:56 +0100

    submission for the task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+""" A module that defines a square class and initializes its fields """
+
+
+class Square:
+    """ a class that defines a square """
+    def __init__(self, size=0):
+        self.__size = size
