71aaf620dbb2f852b2bb73c6453629fa106a7527
HazelDaniel <hazeldaniel856@gmail.com>
2023-07-10 18:30:12 +0100

    submission for the task 3

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""a module where in a function that determines polymorphicly\
+        if a class is an instance of another class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """a function that determines polymorphicly if a class is an\
+            instance of another class"""
+    return (isinstance(obj, a_class))
