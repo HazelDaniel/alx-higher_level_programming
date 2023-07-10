11b1a43bf52af3eaa9bc68c3c2a15360953e6402
HazelDaniel <hazeldaniel856@gmail.com>
2023-07-10 18:49:21 +0100

    re-submission for task 7

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -5,9 +5,11 @@
 class BaseGeometry:
     """an empty class called BaseGeometry"""
     def area(self):
+        """a public instance method that computes the area"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """a public instance method that validates the input"""
         if type(value) is not int:
             raise TypeError("<name> must be an integer")
         if value < 0:
