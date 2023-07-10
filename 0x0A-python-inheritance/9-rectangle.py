6bc48c67a66214f2a50b55db5b5858fa724b76e9
HazelDaniel <hazeldaniel856@gmail.com>
2023-07-10 19:11:06 +0100

    submission for task 9

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""a module that defines a rectangle class"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """a rectangle class that inherits from a 'BaseGeometry' class"""
+    def __init__(self, width, height):
+        """the constructor function"""
+        super().__init__()
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """a public instance method that defines the area"""
+        return (self.__width * self.__height)
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
