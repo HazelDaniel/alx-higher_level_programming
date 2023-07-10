604c9ff6904dd96c67badd523561332eb57c821b
HazelDaniel <hazeldaniel856@gmail.com>
2023-07-10 18:37:31 +0100

    submission for task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+""" a module wherein a function that checks if a\
+        class inherited from another class"""
+
+
+def inherits_from(obj, a_class):
+    """a function that determines if a function \
+           inherited from another class"""
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
