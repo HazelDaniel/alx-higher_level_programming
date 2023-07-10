0c4e122ed7cc8af5db1aa9ecbc1cf867cfca4ab6
HazelDaniel <hazeldaniel856@gmail.com>
2023-07-10 19:14:30 +0100

    submission for task 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""a module that defines a square class"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """a class that defines a square"""
+    def __init__(self, size):
+        """the constructor function"""
+        self.integer_validator("size", size)
+        self.__size = size
