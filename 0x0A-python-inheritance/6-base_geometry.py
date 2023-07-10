292d1c2870857599dd5f1db77e59bbd0eb5b7694
HazelDaniel <hazeldaniel856@gmail.com>
2023-07-10 18:43:25 +0100

    submission for task 6

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""a module that defines an empty class"""
+
+
+class BaseGeometry:
+    """an empty class called BaseGeometry"""
+    def area(self):
+        raise Exception("area() is not implemented")
