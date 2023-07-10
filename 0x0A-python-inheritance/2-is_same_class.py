01b32ba917e3bb28da9fe8a39baf640c4c47f18e
HazelDaniel <hazeldaniel856@gmail.com>
2023-07-10 18:27:31 +0100

    submission for the task 2

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""a module wherein a function that checks for instances is defined"""
+
+
+def is_same_class(obj, a_class):
+    """a function that checks if a class is the same\
+            class as an existing class"""
+    return (isinstance(obj, a_class) and obj.__class__ is a_class)
