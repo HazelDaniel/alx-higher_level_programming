404e1cfb8a9715642541ca263b704ba2d73db17a
HazelDaniel <hazeldaniel856@gmail.com>
2023-07-10 18:12:35 +0100

    submission for the task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""a module that defines a list subclass"""
+
+
+class MyList(list):
+    """a list class that extends from the list base object"""
+
+    def __init__(self):
+        """the constructor function"""
+        list.__init__(self)
+
+    def print_sorted(self):
+        """a function that prints the list in a sorted order"""
+        print(sorted(self))
