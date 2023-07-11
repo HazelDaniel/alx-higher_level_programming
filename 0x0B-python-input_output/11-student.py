dc08e47f5314dd2381e24dbf587275d0c4dd1167
HazelDaniel <hazeldaniel856@gmail.com>
2023-07-11 21:32:09 +0100

    submission for task 12

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""a module that defines a class Student"""
+
+
+class Student:
+    """a student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """the constructor function"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """a public instance method that sets a dictionary representation\
+            of the Student class based on some conditions """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """a public instance method that replaces all\
+            attributes of the Student"""
+        for k, v in json.items():
+            setattr(self, k, v)
--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""a module that defines a Pascal's Triangle function"""
+
+
+def pascal_triangle(n):
+    """A Pascal's Triangle class of max size n"""
+    if n <= 0:
+        return []
+
+    triangles = [[1]]
+    while len(triangles) != n:
+        tr = triangles[-1]
+        tmp = [1]
+        for i in range(len(tr) - 1):
+            tmp.append(tr[i] + tr[i + 1])
+        tmp.append(1)
+        triangles.append(tmp)
+    return triangles
