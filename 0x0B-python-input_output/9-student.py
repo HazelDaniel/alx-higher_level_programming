953a5f68edac5519e7a5f3f73e3a72b8fd8a3680
HazelDaniel <hazeldaniel856@gmail.com>
2023-07-11 21:24:03 +0100

    submission for task 9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""a module that defines a class Student"""
+
+
+class Student:
+    """a student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """the constructor function"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """a public instance method to retrieve a dictionary\
+            representation of the Student class"""
+        return self.__dict__
