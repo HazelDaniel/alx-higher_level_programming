bca035886003d52d6c326a03bcdf6c341313eed6
HazelDaniel <hazeldaniel856@gmail.com>
2023-07-11 20:48:30 +0100

    submission for task 6

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""a module that defines a function that creates an object from a JSON file"""
+import json
+
+
+def load_from_json_file(filename):
+    """a function that creates an object from a JSON file"""
+    with open(filename, mode="r+", encoding="utf-8") as file:
+        return json.load(file)
