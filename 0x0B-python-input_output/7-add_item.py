362de9cb14cbda3f62e2bc572e74be6abbc71b5c
HazelDaniel <hazeldaniel856@gmail.com>
2023-07-11 21:10:37 +0100

    submission for task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""a module that defines a function that adds all command\
+    line arguments to a pyton list and writes them to a file"""
+import json
+import sys
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+if __name__ == "__main__":
+    list = []
+    for arg in sys.argv[1:]:
+        list.append(arg)
+    save_to_json_file(list, "add_item.json")
