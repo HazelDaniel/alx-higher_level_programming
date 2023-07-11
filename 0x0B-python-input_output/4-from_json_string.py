433cc400f04a9e61ce6b581dcd84b4b23c207959
HazelDaniel <hazeldaniel856@gmail.com>
2023-07-11 18:53:09 +0100

    submission for task 4 and 5

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""a module that defines a function that returns an object\
+        represented by a json string"""
+import json
+
+
+def from_json_string(my_str):
+    """a function that returns an object represented by a json string"""
+    return json.loads(my_str)
--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""a module that defines a function that writes an object\
+    to a text file using a JSON representation"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """a function that writes an object\
+        to a text file using a JSON representation"""
+    with open(filename, mode="w", encoding="utf-8") as file:
+        return json.dump(my_obj, file)
