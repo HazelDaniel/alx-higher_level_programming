da9114c4a218f6b93d84b5ff0f23c1998f7647fa
HazelDaniel <hazeldaniel856@gmail.com>
2023-08-15 20:38:20 +0100

    submission for task: 7

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,5 @@
+/*
+ this inserts a new row in the table first_table
+ with database name passed as argument of the mysql command
+*/
+INSERT INTO first_table (`id`, `name`) VALUES (89, "Holberton School");
