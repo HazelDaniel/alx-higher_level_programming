aa9e05c2724f0a36856cd545ba9e0e5678638708
HazelDaniel <hazeldaniel856@gmail.com>
2023-08-15 21:17:11 +0100

    re-submission for tasks 10 - 12

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,5 +1,3 @@
-/*
- this lists all records of the table second_table
- and displays both score and name, and database is passed as an argument of the mysql command
-*/
+-- this lists all records of the table second_table
+-- and displays both score and name, and database is passed as an argument of the mysql command
 SELECT score, name FROM second_table ORDER BY score DESC;
--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,5 +1,3 @@
-/*
- this updates the score of Bob to 10 in the table second_table
- with database name passed as an argument of the mysql command
-*/
+-- this updates the score of Bob to 10 in the table second_table
+-- with database name passed as an argument of the mysql command
 UPDATE second_table SET score = 10 WHERE name = "Bob";
