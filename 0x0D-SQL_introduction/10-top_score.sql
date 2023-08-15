e3f5e25eb7bd1371ab1efe4f6489c5a2f18a7615
HazelDaniel <hazeldaniel856@gmail.com>
2023-08-15 20:48:44 +0100

    submission for task: 10

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+/*
+ this lists all records of the table second_table
+ and displays both score and name, and database is passed as an argument of the mysql command
+*/
+SELECT score, name FROM second_table ORDER BY score DESC;
