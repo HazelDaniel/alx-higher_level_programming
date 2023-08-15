1859f6ad22d3f2e476e9e7382182dd727640dad7
HazelDaniel <hazeldaniel856@gmail.com>
2023-08-15 21:05:31 +0100

    submission for task 100

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,11 @@
+/*
+ this changes a property of a database in the server
+*/
+ALTER DATABASE hbtn_0c_0
+    CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+/*
+ this changes the properties of a table in the database
+*/
+USE hbtn_0c_0;
+ALTER TABLE first_table
+    CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
