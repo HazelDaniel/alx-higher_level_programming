221ae10d62ddbf5af83d9fbcf91b398d13d7aa3a
HazelDaniel <hazeldaniel856@gmail.com>
2023-08-15 20:50:32 +0100

    submission for task: 11

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,5 @@
+/*
+ this lists all records with a score >= 10 in table second_table,
+ display both the score and the name - database is passed as an argument of the mysql command
+*/
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
