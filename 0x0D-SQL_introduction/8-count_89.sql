7af79a60215d804b018c521c68ac6c4058c7778a
HazelDaniel <hazeldaniel856@gmail.com>
2023-08-15 20:45:04 +0100

    submission for task: 8

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,5 @@
+/*
+ this displays the number of records with id = 89 in the table first_table
+ with database name passed as an argument of mysql command
+*/
+SELECT COUNT(*) FROM first_table WHERE id = 89;
