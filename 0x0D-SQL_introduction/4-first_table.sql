16b9989e9c925145847b94105fcf92acae46a63a
HazelDaniel <hazeldaniel856@gmail.com>
2023-08-15 20:33:39 +0100

    submission for task: 4

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,7 @@
+/*
+ this creates a table called first_table in the current database
+ database will be passed as argument of the command
+*/
+CREATE TABLE IF NOT EXISTS first_table (id INT,
+		name VARCHAR(256)
+		);
