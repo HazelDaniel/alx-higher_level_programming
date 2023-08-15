804e2d67e99ee4a672c04bdc8336387c865c86d4
HazelDaniel <hazeldaniel856@gmail.com>
2023-08-15 21:12:22 +0100

    re-submission for task 7

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,5 +1,3 @@
-/*
- this inserts a new row in the table first_table
- with database name passed as argument of the mysql command
-*/
+-- this inserts a new row in the table first_table
+-- with database name passed as argument of the mysql command
 INSERT INTO first_table (`id`, `name`) VALUES (89, "Holberton School");
