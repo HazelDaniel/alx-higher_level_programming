d004f5e3218308e6911a0b0761cc6f5112a9bf11
HazelDaniel <hazeldaniel856@gmail.com>
2023-08-16 20:36:28 +0100

    re-submission for task 12

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,7 +1,7 @@
 -- Lists all shows from hbtn_0d_tvshows_rate by their rating.
 -- Records are ordered by descending rating.
 SELECT `title`, SUM(`rate`) AS `rating`
-  FROM `tv_shows`
+  FROM `tv_shows` AS s_rating
        INNER JOIN `tv_show_ratings` AS s_rating_join
        ON s_rating.`id` = s_rating_join.`show_id`
  GROUP BY `title`
