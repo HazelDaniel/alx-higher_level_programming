5c40d2c8c4defbe17c0d9561d46eb02d7e0d9a9d
HazelDaniel <hazeldaniel856@gmail.com>
2023-09-17 07:27:32 +0100

    submission for task 101

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" prints the State object with the name passed as argument from the database
+""" a module that prints the State objects stored in the DB
+    ordered by id
 """
 import sys
 from relationship_state import Base, State
@@ -8,13 +9,13 @@ from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import relationship
 
-
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
+
     for instance in session.query(State).order_by(State.id):
         print(instance.id, instance.name, sep=": ")
         for city_ins in instance.cities:
