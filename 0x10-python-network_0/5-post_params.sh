e3e76de8eae42dd024f404ddd9532325ed8d3db3
HazelDaniel <hazeldaniel856@gmail.com>
2023-09-28 23:03:32 +0100

    submission for task 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-#this script sends a post request to an endpoint using curl while setting the
-#body using variables
-URL="$1"
-EMAIL="test@gmail.com"
-SUBJECT="I will always be here for PLD"
-
-curl -X POST -d "email=$EMAIL&subject=$SUBJECT" -s "$URL"
+#this script sends a post request to an endpoint using curl while setting the body using variables
+curl -X POST -d "email='test@gmail.com'&subject='I will always be here for PLD'" -s "$URL"
