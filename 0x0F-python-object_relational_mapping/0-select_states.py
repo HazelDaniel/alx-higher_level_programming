99fd29dde930457602d3c96206c0c5a308883f04
HazelDaniel <hazeldaniel856@gmail.com>
2023-11-18 08:42:11 +0100

    re-submission for task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,7 +7,6 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("USE hbtn_0e_0_usa")
     cur.execute("SELECT * FROM states")
     rows = cur.fetchall()
     for row in rows:
