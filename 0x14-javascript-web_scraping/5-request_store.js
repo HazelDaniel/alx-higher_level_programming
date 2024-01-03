d6da66f2e5dfa3c2ea91f39b309cb7c6b91921c9
HazelDaniel <hazeldaniel856@gmail.com>
2024-01-03 18:08:09 +0100

    task-5: re-submission

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -16,7 +16,7 @@ let filePath = cmdArgs[1];
 
 const writeFileAsync = (filePath, content, encoding) => {
   return new Promise((resolve, reject) => {
-    fs.writeFile(filePath, `${content}\n`, encoding, (err, data) => {
+    fs.writeFile(filePath, `${content}`, encoding, (err, data) => {
       if (err) {
         reject(err);
       } else {
